1956a5cc685278c5fa2a094b4c6d5ba1a373d802
Alexandre Marcolino Alves <alexandre.alves.net@gmail.com>
2023-04-20 09:52:54 -0300

    Solução da instalação da lib nlpnet do Pylinguistics com sudo apt-get install python3.7-dev

--- pylinguistics/pylinguistics/run_metrica_exp03_transformers.py
+++ pylinguistics/pylinguistics/run_metrica_exp03_transformers.py
@@ -1,9 +1,19 @@
-run_metrica_exp01_muss_en.py### GERA AS MÉTRICAS DE LEGIBILIDADE COM BASE NOS ARQUIVOS *.br e *.sp Original e Simplificado ###
+### GERA AS MÉTRICAS DE LEGIBILIDADE COM BASE NOS ARQUIVOS *.br e *.sp Original e Simplificado ###
 
 from metrica_ementa import MetricaEmenta
 
-# Experimento 01
-metrica_ementa = MetricaEmenta("exp04_100_ementas_sentencas_512_STF")
-metrica_ementa.generate_metrics_by_ementa("exp04_metricas_ementas_originais_STF.xlsx", "/*.br")
-metrica_ementa.generate_metrics_by_ementa("exp04_metricas_ementas_simplificadas_STF.xlsx", "/*.sp")
+# Experimento 03
+path = '/home/alexandre/Desenvolvimento/mpes-experimentos/transformers/ementas/GERADAS/'
+path_stf = path + 'STF'
+path_trf5 = path + 'TRF5'
+
+# EMENTAS STF
+metrica_ementa = MetricaEmenta(path_stf)
+metrica_ementa.generate_metrics_by_ementa("exp03_metricas_ementas_originais_STF.xlsx", "/*.br")
+metrica_ementa.generate_metrics_by_ementa("exp03_metricas_ementas_simplificadas_STF.xlsx", "/*.sp")
+
+# EMENTAS TRF5
+metrica_ementa = MetricaEmenta(path_trf5)
+metrica_ementa.generate_metrics_by_ementa("exp03_metricas_ementas_originais_TRF5.xlsx", "/*.br")
+metrica_ementa.generate_metrics_by_ementa("exp03_metricas_ementas_simplificadas_TRF5.xlsx", "/*.sp")
 
